dceb135a2a5fcd4520c81ac67fba3840275cf4b0
Zuzana Halgasova <zuzana.halgasova@betterask.erni>
2023-11-18 19:36:41 +0100

    get rid of comment

--- src/app/environments/environment.ts
+++ src/app/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
-    apiUrl: 'https://api.thedogapi.com/v1', //?limit=10
+    apiUrl: 'https://api.thedogapi.com/v1',
     apiKey: 'live_SuYPHKLerg5vyOCBxpw08WTn8UWjmClTAyDquG1l3MV3uhuXMXcKAn51T4Un2Q0m'
 }
